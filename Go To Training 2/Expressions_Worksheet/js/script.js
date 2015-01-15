aa545605b04786406179c1d3acfd773b4ecaf4cc
grattisgray <mggray@fullsail.edu>
2015-01-15 09:29:48 -0600

    Sparky's Age Calculator
    coded

--- Go To Training 2/Expressions_Worksheet/js/script.js
+++ Go To Training 2/Expressions_Worksheet/js/script.js
@@ -1,3 +1,13 @@
 //Michael Gray 1-15-15 Expressions Worksheet
 
+//Converting Dog Years to Human Years
 
+var dogYears = 7;
+var humanYears = 1;
+
+var convertion = dogYears * humanYears;
+
+var humanYears = prompt("Please enter Sparky\'s age in human years:");
+
+alert(dogYears * humanYears);
+result();
