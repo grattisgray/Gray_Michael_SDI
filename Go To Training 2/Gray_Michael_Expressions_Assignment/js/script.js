bb8c854b78fb453c2f4d5865cae88cccd3a9ea6e
grattisgray <mggray@fullsail.edu>
2015-01-15 16:12:04 -0600

    Averaging Grades
    Test Average achieved

--- Go To Training 2/Gray_Michael_Expressions_Assignment/js/script.js
+++ Go To Training 2/Gray_Michael_Expressions_Assignment/js/script.js
@@ -3,3 +3,38 @@
 //Calculating the Average of a grade
 
 
+//Quiz Scores and their raw score
+
+//var quizGrade1 = prompt("Please enter each quiz grade individually");
+
+//var quizGrade2 = prompt("Please enter then next quiz grade");
+
+//var quizGrade3 = prompt("Please enter then next quiz grade");
+
+//var quizScore = Number(quizGrade1) + Number(quizGrade2) + Number(quizGrade3);
+
+//alert(quizScore);
+
+//var quizAvg = quizScore;
+
+
+
+//Test Scores and their raw score
+var testGrade1 = prompt("Please enter each test grade individually");
+
+var testGrade2 = prompt("Please enter then next test grade");
+
+var testGrade3 = prompt("Please enter then next test grade");
+
+var testScore = Number(testGrade1) + Number(testGrade2) + Number(testGrade3);
+
+//alert(testScore);
+
+//Test Average
+
+var testAvg = ((testScore / 3) * 3);
+
+var result = "The Test Average" + testAvg;
+
+alert(result);
+
