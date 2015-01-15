6d2a420808a49ec833fba0764aebe3a112f9e615
grattisgray <mggray@fullsail.edu>
2015-01-15 10:09:57 -0600

    Slice of Pice Part 1
    per person code

--- Go To Training 2/Expressions_Worksheet/js/script.js
+++ Go To Training 2/Expressions_Worksheet/js/script.js
@@ -3,14 +3,24 @@
 //Converting Dog Years to Human Years
 
 // Defining the variables
-var dogYears = 7;
-var humanYears = 1;
+//var dogYears = 7;
+//var humanYears = 1;
 
 // User input prompt and
-var humanYears = prompt("To Calculate Sparky\'s Age \nPlease enter Sparky\'s age in human years:");
+//var humanYears = prompt("To Calculate Sparky\'s Age \nPlease enter Sparky\'s age in human years:");
 //Calculation formula for the conversion
-var conversion = dogYears * humanYears;
+//var conversion = dogYears * humanYears;
 //Calculated total
-alert(conversion);
+//alert(conversion);
 //print out the output of Sparky's age
-console.log(conversion);
+//console.log(conversion);
+
+//Slice of Pie Part 1
+
+var slicesPizza = 8;
+
+var pizzaOrder = prompt("To Calculate the number of pizza slices per person \nPlease enter the number of pizzas ordered");
+
+var peopleParty = prompt("Please enter the number of people at the party");
+
+var pizzaSlice = alert((pizzaOrder * peopleParty)/slicesPizza);
