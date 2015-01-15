562d4610dfaf88136113d68690de0829cc6ecaed
grattisgray <mggray@fullsail.edu>
2015-01-15 13:46:20 -0600

    Slice of Pie
    Part I with bug

--- Go To Training 2/Expressions_Worksheet/js/script.js
+++ Go To Training 2/Expressions_Worksheet/js/script.js
@@ -3,24 +3,32 @@
 //Converting Dog Years to Human Years
 
 // Defining the variables
-//var dogYears = 7;
-//var humanYears = 1;
+var dogYears = 7;
+var humanYears = 1;
 
 // User input prompt and
-//var humanYears = prompt("To Calculate Sparky\'s Age \nPlease enter Sparky\'s age in human years:");
+var humanYears = prompt("To Calculate Sparky\'s Age \nPlease enter Sparky\'s age in human years:");
 //Calculation formula for the conversion
-//var conversion = dogYears * humanYears;
+var conversion = dogYears * humanYears;
 //Calculated total
-//alert(conversion);
+alert(conversion);
 //print out the output of Sparky's age
-//console.log(conversion);
+console.log(conversion);
 
 //Slice of Pie Part 1
 
+// Definition of slices per pizza
 var slicesPizza = 8;
 
+// Needed user input of how many pizzas have been ordered.
 var pizzaOrder = prompt("To Calculate the number of pizza slices per person \nPlease enter the number of pizzas ordered");
 
+// Number of People at this party needs to be defined.
 var peopleParty = prompt("Please enter the number of people at the party");
 
-var pizzaSlice = alert((pizzaOrder * peopleParty)/slicesPizza);
+//calculation of the number of slices multiply the number of pizzas ordered by the number of slices per pizza and divide that number by the people
+var slices = alert((pizzaOrder * slicesPizza)/peopleParty);
+
+//Print out of the number of pizza slices per person at the party
+console.log(slices);
+//print out in the console says "undefined"; I do not know how to debug this problem.
