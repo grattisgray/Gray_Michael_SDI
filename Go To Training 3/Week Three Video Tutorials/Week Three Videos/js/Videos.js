793b5a99e1ccdf10558d4204536b36bd11a11fdf
grattisgray <mggray@fullsail.edu>
2015-01-19 16:01:58 -0600

    Conditional Logic Concepts
    Setup

---
+++ Go To Training 3/Week Three Video Tutorials/Week Three Videos/js/Videos.js
@@ -0,0 +1,15 @@
+/**
+ * Micahel Gray
+ * Section #3
+ * Week Three Videos
+ * 1/19/15
+ */
+
+if(sunny) {
+    goToBeach();
+    if(warmWater){
+        wearNewSuit();
+    }
+}else{
+    goToTheMovies();
+}
