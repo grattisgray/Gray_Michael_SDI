16b5d3e858a772fb6d0a082a04770da2b544255d
grattisgray <mggray@fullsail.edu>
2015-01-19 16:51:50 -0600

    Else if
    three blocks of code

--- Go To Training 3/Week Three Video Tutorials/Week Three Videos/js/Videos.js
+++ Go To Training 3/Week Three Video Tutorials/Week Three Videos/js/Videos.js
@@ -44,8 +44,8 @@ if(kidHeight + sneakerLift > minHeight){
 */
 
 //If and else
-//Choosing between two
-
+//Choosing between two blocks of code
+/*
 var kidHeight = 36;
 var minHeight = 48;
 
@@ -57,5 +57,30 @@ if(kidHeight >= minHeight){
     console.log("You can ride the coaster!");
 }else{
     //code performed if condition is false
+    console.log("Sorry kid, you've got some growing to do first!");
+}
+/*
+ */
+
+//Else If
+//Choosing between three or more blocks of code
+//always requires a condition
+
+var kidHeight = 46;
+var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+
+if(kidHeight >= minHeight){
+    //you can ride!
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
+}else{
+    //sorry you have growing to do
     console.log("Sorry kid, you've got some growing to do first!");
 }
