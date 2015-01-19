787db68627648d90dd3956c3c03aced84ce2b3c9
grattisgray <mggray@fullsail.edu>
2015-01-19 16:11:54 -0600

    Conditionals Example
    Screencast

--- Go To Training 3/Week Three Video Tutorials/Week Three Videos/js/Videos.js
+++ Go To Training 3/Week Three Video Tutorials/Week Three Videos/js/Videos.js
@@ -1,15 +1,30 @@
 /**
  * Micahel Gray
- * Section #3
+ * SDI Section #3
  * Week Three Videos
  * 1/19/15
  */
 
-if(sunny) {
-    goToBeach();
-    if(warmWater){
-        wearNewSuit();
-    }
-}else{
-    goToTheMovies();
+//if(sunny) {
+ //   goToBeach();
+  //  if(warmWater){
+  //      wearNewSuit();
+ //   }
+//}else{
+//    goToTheMovies();
+//}
+
+//Basic Conditional Statement
+//Decision - making for the Machine
+
+//Conditonal Logic
+
+var oldEnough = true;
+
+//If the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+  //code performed if condition is true
+    console.log("You can ride the coaster!");
 }
+console.log("What comes after.");
+
