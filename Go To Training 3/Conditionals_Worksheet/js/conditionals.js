8dcb21ec281d01c03e6c901b89bce7161b5827b5
grattisgray <mggray@fullsail.edu>
2015-01-21 08:51:07 -0600

    Conditionals Worksheet
    Setup and Part One

---
+++ Go To Training 3/Conditionals_Worksheet/js/conditionals.js
@@ -0,0 +1,17 @@
+/**
+ * Michael Gray
+ * SDI Section #3
+ * Conditionals Worksheet
+ * 1/20/15
+ */
+
+var heavyWeight = prompt("How much do you weight");
+
+//Determine whether an entrant qualifies based on his weight.
+if(heavyWeight >= 250){
+    console.log("Your are heavy enough to compete")
+}else{
+    if(heavyWeight < 250)
+    console.log("You need to gain some weight.");
+}
+
