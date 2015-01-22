be02afdb1b49ee2b255307d2390325963fb361f2
grattisgray <mggray@fullsail.edu>
2015-01-22 15:38:55 -0600

    Learned about length and access individual items
    length and access individual items

--- Go To Training 3/Go To Training Day #3/js/GTT3.js
+++ Go To Training 3/Go To Training Day #3/js/GTT3.js
@@ -5,3 +5,18 @@
  * GoTo Training Day #3
  */
 
+//Check how many pears are in a fruit bowl
+//Create our "Fruit bowl"
+//Use an array, variable that holds multiple variables
+var bowlOfFruit = ["apple", "pear", "peach"];
+
+console.log(bowlOfFruit);
+
+//Find out how many items are in an array.
+//length property - how many items in an array
+//Dot Syntax - "use a period"
+console.log(bowlOfFruit.length);
+
+//Call upon a specific item in the array
+console.log(bowlOfFruit[2]);
+
