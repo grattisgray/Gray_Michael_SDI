a03efe842c8d38123838a4d144341ccb69c4d9ad
grattisgray <mggray@fullsail.edu>
2015-01-22 16:08:43 -0600

    Tested 3 items for pear
    pears

--- Go To Training 3/Go To Training Day #3/js/GTT3.js
+++ Go To Training 3/Go To Training Day #3/js/GTT3.js
@@ -20,3 +20,58 @@ console.log(bowlOfFruit.length);
 //Call upon a specific item in the array
 console.log(bowlOfFruit[2]);
 
+//Create a variable to hold the total number of pears
+var totalNumPears = 0;
+
+// "6" == 6 TRUE - Only looks at the value
+// "6" === 6 FALSE Data type is different
+
+//if(condition to test){Code that will run if condition is true }
+
+/*
+If Else statement
+if(condition){
+    Code to run if condition is true
+}else{
+    Code to run if condition is false
+}
+
+ */
+
+if(bowlOfFruit[0] === "peach"   ){
+    //Count the fruit as a peach
+    //add one to the total num of pears
+    console.log("This is a pear!");
+    totalNumPears++; //totalNumPears = totalNumPears + 1;
+}else{
+    //Code to run if not a pear
+    console.log("This fruit is not a pear.");
+}
+
+//Test the next item in the array
+
+
+if(bowlOfFruit[1] === "pear"   ){
+    //Count the fruit as a peach
+    //add one to the total num of pears
+    console.log("This is a pear!");
+    totalNumPears++; //totalNumPears = totalNumPears + 1;
+}else{
+    //Code to run if not a pear
+    console.log("This fruit is not a pear.");
+}
+
+console.log("There are "+totalNumPears + " pear(s) in the fruit bowl.");
+
+
+//Test third item in the array
+
+if(bowlOfFruit[2] === "pear"   ){
+    //Count the fruit as a peach
+    //add one to the total num of pears
+    console.log("This is a pear!");
+    totalNumPears++; //totalNumPears = totalNumPears + 1;
+}else{
+    //Code to run if not a pear
+    console.log("This fruit is not a pear.");
+}
