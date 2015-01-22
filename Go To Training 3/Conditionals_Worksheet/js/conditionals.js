86558fab8370d291f40abb3216b518247afd9562
grattisgray <mggray@fullsail.edu>
2015-01-22 15:30:55 -0600

    Conditionals Worsheet
    Check the Login - cannot figure out how to code this.

--- Go To Training 3/Conditionals_Worksheet/js/conditionals.js
+++ Go To Training 3/Conditionals_Worksheet/js/conditionals.js
@@ -19,12 +19,12 @@ if(heavyWeight >= 250){
 */
 //Group 1: Expressions with Conditionals
 //Last Chance for Gas
-
-//Define the varibles with User prompts.
+/*
+//Define the variables with User prompts.
 var carEfficiency = prompt("What is your car's MPG?");
 var carTank = prompt("How many gallons of gas does your car hold?");
 var carGasPercent = prompt("What percentage of gas do you have remaining?");
-//Varibles must be in the right order to work.
+//Variables must be in the right order to work.
 var carGallons = ((carGasPercent * .01) * carTank);
 var gasPercent = (carGasPercent / 100);
 
@@ -39,8 +39,27 @@ if((carEfficiency * carTank) * gasPercent >= 200) {
         console.log("You only have " + carGallons + " gallons of gas in your tank, better get gas now while you can!");
 }
 
-
+*/
 
 //Group 2: Multiple Results
 //Check the Login
 
+//Define the variables with User prompts
+var userName = prompt("Please enter your User Name.");
+userName === "GrattisGray";
+var userPassword = prompt("Please enter the your password.");
+userPassword === "Gray14";
+
+//Define the variables that are used to calculate
+var userName1 = "Michael Gray";
+
+if(userName){
+    (userPassword);{
+        if(userPassword = Gray14);
+        alert("Welcome, " + userName1);
+        console.log("Weclome, " + userName1);
+    }
+
+}else{
+
+}
