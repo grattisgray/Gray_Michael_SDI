e9bb99fdee319ff047641d59fc504aae8770fa6e
grattisgray <mggray@fullsail.edu>
2015-01-27 21:14:11 -0600

    Functions Assignment
    Calculating account balance

--- Gray_Michael_Functions_Assignment/js/functions.js
+++ Gray_Michael_Functions_Assignment/js/functions.js
@@ -5,4 +5,26 @@
  * Functions Assignment
 */
 
-alert("Hello");
+//Calculate
+/*
+Check account balance
+Deduct purchases
+Add deposits
+Calculate quarterly interest on your account
+ */
+
+//Defining of the variables
+var account = prompt("What is your current account balance?");
+var purchase = prompt("How much did you spend?");
+var deposit = prompt("How much did you deposit?");
+
+
+//arguments
+var current = accountChange(account,purchase,deposit);
+//parameters for calculating current account summary
+function accountChange(account,purchase,deposit){
+    //Return the values for account total
+    return Number(account) + Number(deposit) - Number(purchase);
+}
+//Print out of the the account total for that day.
+console.log("Your current account balance is $ " + current);
